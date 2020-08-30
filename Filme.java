5c9654b3725bbe937e437dba5b0c0ce2468f0c9c
Guilherme Pongeti <gui22pongeti@gmail.com>
2020-08-30 13:48:36 +0000

    Initial commit

---
+++ Filme.java
@@ -0,0 +1,31 @@
+class Filme{
+
+  private String titulo;
+  private int ano;
+  private int tipo;
+
+  public void setTitulo(String titu){
+       this.titulo = tit;   
+  }
+
+  public String getTitulo(){
+    return this.titulo;
+  }
+
+  public void setAno(int an){
+    this.ano = an;
+  }
+
+  public int getAno(){
+    return this.ano;
+  }
+
+  
+  public void setTipo(int tip){
+    this.tipo = tip;
+  }
+
+  public int getTipo(){
+    return this.tipo;
+  }
+}
